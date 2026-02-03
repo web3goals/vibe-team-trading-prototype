bd3659a23c717ee4859bc45229e0768c487cf27e
kiv1n <aloha@kiv1n.ru>
2026-02-03 19:57:16 +0300

    Update session duration in yellowConfig to 24 hours

--- app/config/yellow.ts
+++ app/config/yellow.ts
@@ -1,7 +1,7 @@
 export const yellowConfig = {
   url: "wss://clearnet-sandbox.yellow.com/ws",
   appName: "Test app",
-  sessionDuration: 3600, // 1 hour
+  sessionDuration: 24 * 60 * 60, // 24 hours
   sessionScope: "test.app",
   sessionAllowances: [{ asset: "ytest.usd", amount: "1000000000" }],
 };
