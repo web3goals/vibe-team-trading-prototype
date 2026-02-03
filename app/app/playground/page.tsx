9a2f3dc90dc0adc5596ddca3c16962826aad1c3b
kiv1n <aloha@kiv1n.ru>
2026-02-03 22:20:14 +0300

    Implement PATCH API for adding yellow message signatures and enhance PlaygroundPage with new functionality

--- app/app/playground/page.tsx
+++ app/app/playground/page.tsx
@@ -4,11 +4,12 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { demoConfig } from "@/config/demo";
 import { yellowConfig } from "@/config/yellow";
-import { GroupAgent, GroupUser } from "@/types/group";
+import { GroupAgent, GroupMessage, GroupUser } from "@/types/group";
 import {
   createECDSAMessageSigner,
   RPCAppDefinition,
   RPCAppSessionAllocation,
+  RPCData,
   RPCProtocolVersion,
 } from "@erc7824/nitrolite";
 import { createAppSessionMessage as createCreateAppSessionMessage } from "@erc7824/nitrolite/dist/rpc/api";
@@ -17,11 +18,12 @@ import { useState } from "react";
 
 export default function PlaygroundPage() {
   const [address, setAddress] = useState<string | undefined>();
+  const [ensName, setEnsName] = useState<string | undefined>();
   const [sessionAccountPrivateKey, setSessionAccountPrivateKey] = useState<
     string | undefined
   >();
 
-  async function signIn(address: string) {
+  async function signIn(address: string, ensName: string) {
     console.log(`Signing in, address: ${address}...`);
 
     const { data } = await axios.post("/api/yellow/session-account", {
@@ -30,6 +32,7 @@ export default function PlaygroundPage() {
     const sessionAccount = data.data.sessionAccount;
 
     setAddress(address);
+    setEnsName(ensName);
     setSessionAccountPrivateKey(sessionAccount.privateKey);
 
     console.log("Signing in completed");
@@ -121,6 +124,39 @@ export default function PlaygroundPage() {
     console.log("Creating group completed");
   }
 
+  async function addYellowMessageSignature() {
+    console.log("Adding yellow message signature...");
+
+    const message: GroupMessage = {
+      id: "6982440b74d49fb886ce5c95",
+      created: new Date("2026-02-03T18:52:59.085Z"),
+      senderAddress: "0x818eD0E13030FDE0C86B771a965084e44CC7F8d6",
+      senderRole: "system",
+      content:
+        'Sign a message to create an Yellow app session, create app session message: {"req":[1770144788547,"create_app_session",{"definition":{"protocol":"NitroRPC/0.4","participants":["0xB418506A0dd0E6c81B2a2901a8aa2B6F409BFB3f","0x818eD0E13030FDE0C86B771a965084e44CC7F8d6","0x568647a8f0dDc1772E97aDD23c70960138F16330","0x60aef32500a838cb6ef895478606a3d2DC0deD7c"],"weights":[0,25,25,25],"quorum":50,"challenge":0,"nonce":1770144778914,"application":"Test app"},"allocations":[{"participant":"0xB418506A0dd0E6c81B2a2901a8aa2B6F409BFB3f","asset":"ytest.usd","amount":"0.0"},{"participant":"0x818eD0E13030FDE0C86B771a965084e44CC7F8d6","asset":"ytest.usd","amount":"100.0"},{"participant":"0x568647a8f0dDc1772E97aDD23c70960138F16330","asset":"ytest.usd","amount":"100.0"},{"participant":"0x60aef32500a838cb6ef895478606a3d2DC0deD7c","asset":"ytest.usd","amount":"100.0"}]},1770144778914],"sig":["0xfbc40abaf83ae257209c1a5abce3528770defe9e2118c56920dab62a16dfae1836f636520c598820dabe9b5a10195e2d2282fa25235412c40fc68882a7caae2b1c"]}',
+      yellowMessage:
+        '{"req":[1770144788547,"create_app_session",{"definition":{"protocol":"NitroRPC/0.4","participants":["0xB418506A0dd0E6c81B2a2901a8aa2B6F409BFB3f","0x818eD0E13030FDE0C86B771a965084e44CC7F8d6","0x568647a8f0dDc1772E97aDD23c70960138F16330","0x60aef32500a838cb6ef895478606a3d2DC0deD7c"],"weights":[0,25,25,25],"quorum":50,"challenge":0,"nonce":1770144778914,"application":"Test app"},"allocations":[{"participant":"0xB418506A0dd0E6c81B2a2901a8aa2B6F409BFB3f","asset":"ytest.usd","amount":"0.0"},{"participant":"0x818eD0E13030FDE0C86B771a965084e44CC7F8d6","asset":"ytest.usd","amount":"100.0"},{"participant":"0x568647a8f0dDc1772E97aDD23c70960138F16330","asset":"ytest.usd","amount":"100.0"},{"participant":"0x60aef32500a838cb6ef895478606a3d2DC0deD7c","asset":"ytest.usd","amount":"100.0"}]},1770144778914],"sig":["0xfbc40abaf83ae257209c1a5abce3528770defe9e2118c56920dab62a16dfae1836f636520c598820dabe9b5a10195e2d2282fa25235412c40fc68882a7caae2b1c"]}',
+    };
+
+    // Create Yellow message signer
+    const yellowMessageSigner = createECDSAMessageSigner(
+      sessionAccountPrivateKey as `0x${string}`,
+    );
+    const yellowMessage = message.yellowMessage as string;
+    const yellowMessageJson = JSON.parse(yellowMessage);
+    const signature = await yellowMessageSigner(yellowMessageJson as RPCData);
+
+    await axios.patch(
+      `/api/groups/6982440b74d49fb886ce5c96/messages/${message.id}`,
+      {
+        senderEnsName: ensName,
+        signature: signature,
+      },
+    );
+
+    console.log("Adding yellow message signature completed");
+  }
+
   return (
     <div className="max-w-xl mx-auto px-4 py-8">
       <p className="font-bold">Playground</p>
@@ -130,14 +166,40 @@ export default function PlaygroundPage() {
           Address: {address || "N/A"}
         </p>
         <p className="text-sm text-muted-foreground">
+          ENS Name: {ensName || "N/A"}
+        </p>
+        <p className="text-sm text-muted-foreground">
           Session account private key: {sessionAccountPrivateKey || "N/A"}
         </p>
       </div>
       <div className="flex flex-col items-start gap-2 mt-4">
-        <Button onClick={() => signIn(demoConfig.userA.address)}>
+        <Button
+          onClick={() =>
+            signIn(demoConfig.userA.address, demoConfig.userA.ensName)
+          }
+        >
           Sign in User A
         </Button>
-        <Button onClick={() => createGroup()}>Create Group</Button>
+        <Button
+          onClick={() =>
+            signIn(demoConfig.userB.address, demoConfig.userB.ensName)
+          }
+        >
+          Sign in User B
+        </Button>
+        <Button
+          onClick={() =>
+            signIn(demoConfig.userC.address, demoConfig.userC.ensName)
+          }
+        >
+          Sign in User C
+        </Button>
+        <Button variant="outline" onClick={() => createGroup()}>
+          Create Group
+        </Button>
+        <Button variant="outline" onClick={() => addYellowMessageSignature()}>
+          Add Yellow Message Signature
+        </Button>
       </div>
     </div>
   );
