a000b2c4232bd114ede02775c67384d295ac889b
kiv1n <aloha@kiv1n.ru>
2026-02-05 12:52:23 +0300

    Implement POST endpoint for creating demo trade proposal messages

---
+++ app/app/api/groups/[groupId]/messages/demo/trade-proposal/route.ts
@@ -0,0 +1,85 @@
+import { getAgentYellowMessageSigner } from "@/lib/agent";
+import { createFailedApiResponse, createSuccessApiResponse } from "@/lib/api";
+import { getErrorString } from "@/lib/error";
+import { findGroups, insertOrUpdateGroup } from "@/mongodb/services/group";
+import {
+  createSubmitAppStateMessage,
+  RPCAppSessionAllocation,
+} from "@erc7824/nitrolite";
+import { ObjectId } from "mongodb";
+import { NextRequest } from "next/server";
+
+export async function POST(
+  _request: NextRequest,
+  { params }: { params: Promise<{ groupId: string }> },
+) {
+  try {
+    console.log("[API] Creating demo trade proposal message...");
+
+    const { groupId } = await params;
+
+    // Find the group
+    const group = await findGroups({ id: groupId }).then((groups) => groups[0]);
+    if (!group) {
+      throw new Error(`Group not found with id: ${groupId}`);
+    }
+
+    // Define new Yellow allocations
+    // TODO: Calculate based on the previous state
+    const yellowAppAllocations: RPCAppSessionAllocation[] = [
+      {
+        participant: group.agent.address,
+        asset: "ytest.usd",
+        amount: Number(0 + group.users.length * 1).toString(),
+      },
+      ...group.users.map((user) => ({
+        participant: user.address,
+        asset: "ytest.usd",
+        amount: Number(10 - 1).toString(),
+      })),
+    ];
+
+    // Create Yellow submit app state message signed by the agent
+    const yellowMessageSigner = await getAgentYellowMessageSigner(
+      group.agent.address,
+    );
+    const yellowMessage = await createSubmitAppStateMessage(
+      yellowMessageSigner,
+      {
+        app_session_id: group.yellowAppSessionId as `0x${string}`,
+        allocations: yellowAppAllocations,
+        version: 2,
+      },
+    );
+
+    // Add message to the group and save to database
+    group.messages.push({
+      id: new ObjectId().toString(),
+      created: new Date(),
+      creatorAddress: group.agent.address,
+      creatorRole: "agent",
+      content:
+        "Buying BTC makes sense. I can allocate 1.0 USD from each member, selling when RSI < 30.",
+      extra: {
+        yellow: {
+          message: yellowMessage,
+          messageCreated: new Date(),
+          messageSignerAddresses: [group.agent.address],
+        },
+      },
+    });
+
+    // Update the group in the database
+    await insertOrUpdateGroup(group);
+
+    return createSuccessApiResponse({ group });
+  } catch (error) {
+    console.error(
+      `[API] Failed to create demo trade proposal message, error: ${getErrorString(error)}`,
+    );
+    return createFailedApiResponse(
+      { message: "Internal server error, try again later" },
+      500,
+    );
+  }
+}
