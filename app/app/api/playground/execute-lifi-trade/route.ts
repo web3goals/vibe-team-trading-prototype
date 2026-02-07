695612218a2f1d1908daf863b14fbcb7f7d4e112
kiv1n <aloha@kiv1n.ru>
2026-02-07 09:35:45 +0300

    Implemented automated token swap capabilities by integrating the @lifi/sdk and exposing a new playground API route

---
+++ app/app/api/playground/execute-lifi-trade/route.ts
@@ -0,0 +1,22 @@
+import { demoConfig } from "@/config/demo";
+import { executeLiFiTradeByAgent } from "@/lib/agent";
+import { createFailedApiResponse, createSuccessApiResponse } from "@/lib/api";
+import { getErrorString } from "@/lib/error";
+
+export async function POST() {
+  try {
+    console.log("[API] Executing LI.FI trade...");
+
+    await executeLiFiTradeByAgent(demoConfig.groupAgentA.address);
+
+    return createSuccessApiResponse(42);
+  } catch (error) {
+    console.error(
+      `[API] Failed to execute LI.FI trade, error: ${getErrorString(error)}`,
+    );
+    return createFailedApiResponse(
+      { message: "Internal server error, try again later" },
+      500,
+    );
+  }
+}
--- app/lib/agent.ts
+++ app/lib/agent.ts
@@ -1,9 +1,10 @@
 import { demoConfig } from "@/config/demo";
 import { yellowConfig } from "@/config/yellow";
 import { createECDSAMessageSigner, MessageSigner } from "@erc7824/nitrolite";
+import { ChainId, createConfig, EVM, executeRoute, getRoutes } from "@lifi/sdk";
 import { createWalletClient, getAddress, http } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
-import { sepolia } from "viem/chains";
+import { base, sepolia } from "viem/chains";
 import { Client } from "yellow-ts";
 import { authenticateWalletInYellow } from "./yellow";
 
@@ -54,4 +55,57 @@ export async function sendYellowMessageByAgent(
   yellowClient.disconnect();
 
   return yellowResponse as string;
+}
+
+export async function executeLiFiTradeByAgent(
+  agentAddress: `0x${string}`,
+): Promise<void> {
+  const agentPrivateKey = getAgentPrivateKey(agentAddress);
+  if (!agentPrivateKey) {
+    throw new Error("Agent private key undefined");
+  }
+  const agentAccount = privateKeyToAccount(agentPrivateKey as `0x${string}`);
+  const agentWalletClient = createWalletClient({
+    account: agentAccount,
+    chain: base,
+    transport: http(),
+  });
+
+  createConfig({
+    integrator: process.env.LIFI_INTEGRATOR as string,
+    apiKey: process.env.LIFI_API_KEY as string,
+    routeOptions: {
+      fee: 0.01,
+    },
+    providers: [
+      EVM({
+        getWalletClient: async () => agentWalletClient,
+      }),
+    ],
+  });
+
+  const getRoutesResponse = await getRoutes({
+    fromChainId: ChainId.BAS,
+    toChainId: ChainId.BAS,
+    fromTokenAddress: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913", // USDC
+    toTokenAddress: "0x9f86db9fc6f7c9408e8fda3ff8ce4e78ac7a6b07", // CLAWD
+    fromAmount: "100000", // 0.1 USDC
+    fromAddress: agentAddress, // The address from which the tokens are being transferred
+  });
+  console.log(
+    `[Agent] Get routes response: ${JSON.stringify(getRoutesResponse)}`,
+  );
+
+  const route = getRoutesResponse.routes[0];
+  console.log(`[Agent] Route: ${JSON.stringify(route)}`);
+
+  const executeRouteResponse = await executeRoute(route, {
+    // Gets called once the route object gets new updates
+    updateRouteHook(route) {
+      console.log(`[Agent] Updated route: ${JSON.stringify(route)}`);
+    },
+  });
+  console.log(
+    `[Agent] Execute route response: ${JSON.stringify(executeRouteResponse)}`,
+  );
 }
